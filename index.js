942a5154584dfea533eae53b3f04f55dcf05970e
Cheekay <chikeonyema@gmail.com>
2023-08-08 15:40:08 -0400

    generates new password with 15 random characters

---
+++ index.js
@@ -0,0 +1,113 @@
+const characters = [
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z",
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"~",
+	"`",
+	"!",
+	"@",
+	"#",
+	"$",
+	"%",
+	"^",
+	"&",
+	"*",
+	"(",
+	")",
+	"_",
+	"-",
+	"+",
+	"=",
+	"{",
+	"[",
+	"}",
+	"]",
+	",",
+	"|",
+	":",
+	";",
+	"<",
+	">",
+	".",
+	"?",
+	"/",
+];
+
+let passwordOne = document.getElementById("password-one");
+let passwordTwo = document.getElementById("password-two");
+
+function generatePasswords() {
+	//set the textContent for passwordOne and password two to empty strings
+	passwordOne.textContent = "";
+	passwordTwo.textContent = "";
+
+	//for loop to iterate through the characters array and return 15 random characters
+
+	for (let i = 0; i < 15; i++) {
+		let randomPasswordOne = Math.floor(Math.random() * characters.length);
+		let randomPasswordTwo = Math.floor(Math.random() * characters.length);
+
+		//add each random character to passwordOne and passwordTwo strings
+		passwordOne.textContent += characters[randomPasswordOne];
+		passwordTwo.textContent += characters[randomPasswordTwo];
+	}
+}
